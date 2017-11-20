26e286634371bc6012c9536a92ec8c1e7b0bddd6
LoveMyDouDou <605988578@qq.com>
2017-11-20 19:41:56 +0800

    牛客网初级项目课第三节结束

--- nowstagram/models.py
+++ nowstagram/models.py
@@ -1,23 +1,47 @@
 #coding=utf-8
 
 from nowstagram import db
+from datetime import datetime
 import random
 
+
+class Comment(db.Model):
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    content=db.Column(db.String(1024))
+    image_id=db.Column(db.Integer,db.ForeignKey('image.id'))
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+    status=db.Column(db.Integer,default=0) #0正常 1被删除
+    user=db.relationship('Image')
+
+    def __init__(self,content,image_id,user_id):
+        self.content=content
+        self.image_id=image_id
+        self.user_id=user_id
+
+    def __repr__(self):
+        return '<Comment %d %s>' %(self.id,self.content)
 
 class Image(db.Model):
     id=db.Column(db.Integer,primary_key=True,autoincrement=True)
     url=db.Column(db.String(512))
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
-    create_date=db.Column(db.DateTime)
+    created_date=db.Column(db.DateTime)
+    comments=db.relationship('Comment')
 
-    def __init__(self,url,user):
+    def __init__(self,url,user_id):
+        self.url=url
+        self.user_id=user_id
+        self.created_date=datetime.now()
 
+    def __repr__(self):
+        return '<Image %d %s>' %(self.id,self.url)
 
 class User(db.Model):
     id=db.Column(db.Integer,primary_key=True,autoincrement=True)
     username=db.Column(db.String(80),unique=True)
     password=db.Column(db.String(32))
     head_url=db.Column(db.String(256))
+    images=db.relationship('Image',backref='user',lazy='dynamic')
 
     def __init__(self,username,password):
         self.username=username
